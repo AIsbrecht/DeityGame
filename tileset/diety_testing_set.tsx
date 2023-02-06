8d1c205c0637954bc90be9e3f825fd8be23e0571
Alex Isbrecht <isbrechtalex@gmail.com>
2023-02-06 14:16:18 -0500

    files for graphics added

---
+++ tileset/diety_testing_set.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="diety_testing_set" tilewidth="320" tileheight="320" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="320" height="320" source="Door_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="320" height="320" source="Wall_1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="320" height="320" source="Pillar_1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="320" height="320" source="Floor_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="320" height="320" source="Top_test_wall_1.png"/>
+ </tile>
+</tileset>
